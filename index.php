c84e970b64833e0e1e8b57886f07d77c4a3a539f
Nusratulloh001 <nusratulloh001@gmail.com>
2026-08-30 20:52:29 +0500

    Changed

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
-<?php
-
-
-require "vendor/autoload.php";
-
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Helper\Table;
-
-$output = new ConsoleOutput();
-$table = new Table($output);
-
-// Задаем заголовки колонок
-$table->setHeaders(['ID', 'Имя пользователя', 'Роль', 'Статус']);
-
-// Добавляем строки с данными (можно использовать цветные теги!)
-$table->setRows([
-    ['1', 'Иван Иванов', 'Администратор', '<info>Активен</info>'],
-    ['2', 'Пётр Петров', 'Модератор', '<comment>В отпуске</comment>'],
-    ['3', 'Анна Сидорова', 'Пользователь', '<error>Заблокирован</error>'],
-]);
-
-// Рендерим таблицу в консоль
-$table->render();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: #151515;
+            color: #bdbdbd;
+            font-size: 21px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        require "main.php";
+    ?>
+</body>
+</html>
